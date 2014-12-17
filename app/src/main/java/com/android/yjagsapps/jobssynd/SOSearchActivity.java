b3ebf6d649624a02c5cd60b6aae980ec8669bca8
yaramjags <yaramjags@gmail.com>
2014-12-17 01:25:30 -0800

    Update 1 on 17th Dec 2014

--- app/src/main/java/com/android/yjagsapps/jobssynd/SOSearchActivity.java
+++ app/src/main/java/com/android/yjagsapps/jobssynd/SOSearchActivity.java
@@ -6,6 +6,9 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.yjagsapps.jobssynd.util.ConnectionDetector;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
@@ -14,49 +17,68 @@ import java.net.URLEncoder;
  * Created by Jags on 12/4/2014.
  */
 public class SOSearchActivity extends Activity{
+
+    ConnectionDetector cd;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_jobsearch);
+
+        cd = new ConnectionDetector(getApplicationContext());
     }
 
-    public void listAllJobs(View view){
-        Intent  intent = new Intent(this,SOCareersActivity.class);
+    public void listAllJobs(View view) {
+        Intent intent = new Intent(this, SOCareersActivity.class);
         String message = "";
 
-        //EditText editTextJobSkill = (EditText) findViewById(R.id.TextViewJobSkill);
-        //try {
-        //
-        //    message = URLEncoder.encode(editTextJobSkill.getText().toString(),"UTF-8");
-        //}
-        //catch (UnsupportedEncodingException e){
-        //    Log.e("NOT OK","Some error");
-        //}
-
-        intent.putExtra("searchTerm", message);
-        intent.putExtra("location", message);
-        startActivity(intent);
+        //If there is internet Connection for the device, go to job display activity.
+        if (checkInternetConnection()) {
+            intent.putExtra("searchTerm", message);
+            intent.putExtra("location", message);
+            startActivity(intent);
+        }
+        else
+        {
+            //Display message that there is no internet connection.
+            Toast toast = Toast.makeText(getApplicationContext(), "There is no Internet Connection..Please Try Later..!!",Toast.LENGTH_SHORT);
+            toast.show();
+        }
     }
 
     public void searchJobs(View view){
 
-        Intent  intent = new Intent(this,SOCareersActivity.class);
-        String messageSearchTerm = "";
-        String messageLocation = "";
-        EditText editTextJobKeyword = (EditText) findViewById(R.id.TextViewJobKeyword);
-        EditText editTextJobLocation = (EditText) findViewById(R.id.TextViewJobLocation);
-        try {
+        //If there is internet Connection for the device, go to job display activity.
+        if(checkInternetConnection()) {
+            Intent intent = new Intent(this, SOCareersActivity.class);
+            String messageSearchTerm = "";
+            String messageLocation = "";
+            EditText editTextJobKeyword = (EditText) findViewById(R.id.TextViewJobKeyword);
+            EditText editTextJobLocation = (EditText) findViewById(R.id.TextViewJobLocation);
+            try {
+                messageSearchTerm = URLEncoder.encode(editTextJobKeyword.getText().toString(), "UTF-8");
+                messageLocation = URLEncoder.encode(editTextJobLocation.getText().toString(), "UTF-8");
+            } catch (UnsupportedEncodingException e) {
+                Log.e("NOT OK", "Some error");
+            }
 
-            messageSearchTerm = URLEncoder.encode(editTextJobKeyword.getText().toString(),"UTF-8");
-            messageLocation = URLEncoder.encode(editTextJobLocation.getText().toString(),"UTF-8");
+            intent.putExtra("searchTerm", messageSearchTerm);
+            intent.putExtra("location", messageLocation);
+            startActivity(intent);
         }
-        catch (UnsupportedEncodingException e){
-            Log.e("NOT OK","Some error");
+        else
+        {
+            //Display message that there is no internet connection.
+            Toast toast = Toast.makeText(getApplicationContext(), "There is no Internet Connection..Please Try Later..!!",Toast.LENGTH_SHORT);
+            toast.show();
         }
+    }
 
-        intent.putExtra("searchTerm", messageSearchTerm);
-        intent.putExtra("location", messageLocation);
-        startActivity(intent);
-
+    private boolean checkInternetConnection()
+    {
+        if(cd.isConnectionToInternet())
+            return true;
+        else
+            return false;
     }
 }
---
+++ app/src/main/java/com/android/yjagsapps/jobssynd/util/ConnectionDetector.java
@@ -0,0 +1,36 @@
+package com.android.yjagsapps.jobssynd.util;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+/**
+ * Created by Jags on 12/17/2014.
+ */
+public class ConnectionDetector {
+
+    private Context _context;
+
+    public ConnectionDetector(Context _context) {
+        this._context = _context;
+    }
+
+    public boolean isConnectionToInternet()
+    {
+        ConnectivityManager connectivity = (ConnectivityManager) _context.getSystemService(_context.CONNECTIVITY_SERVICE);
+        if(connectivity != null)
+        {
+            NetworkInfo[] info = connectivity.getAllNetworkInfo();
+            if(info != null){
+                for (int i=0;i<info.length; i++)
+                {
+                    if(info[i].getState() == NetworkInfo.State.CONNECTED)
+                    {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+}
